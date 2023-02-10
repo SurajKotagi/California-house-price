a031732a3a86ba8f17a6d742660717d5044bad2f
Suraj Kotagi <spk1kotagi@gmail.com>
2023-02-10 19:22:16 +0530

    website is ready

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import json
 import pickle
+
+from flask import Flask, request, app, jsonify, url_for,  render_template
 import numpy as np
-from flask import Flask, request, app,jsonify, render_template
 import pandas as pd
 
 app = Flask(__name__)
-reg_model = pickle.load(open('regmodel.pkl', 'rb'))
-scalar = pickle.load(open('scaling.pkl', 'rb'))
 
+regmodel = pickle.load(open('regmodel.pkl', 'rb'))
+scalar = pickle.load(open('scaling.pkl', 'rb'))
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -18,17 +19,17 @@ def predict_api():
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
     new_data = scalar.transform(np.array(list(data.values())).reshape(1,-1))
-    output = reg_model.predict(new_data)
+    output = regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
 
-app.route('/predict', methods = ['POST'])
+@app.route('/predict', methods = ['POST'])
 def predict():
     data = [float(x) for x in request.form.values()]
     final_input = scalar.transform(np.array(data).reshape(1,-1))
     print(final_input)
-    output = reg_model.predict(final_input[0])
+    output = regmodel.predict(final_input)[0]
     return render_template("home.html" , prediction_text = "The House value is {}".format(output))
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug = True)
